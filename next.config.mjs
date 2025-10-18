3fa72fa3e77385a96057b71bd7b2e9607e4e6e0e
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-10-18 16:53:33 -0300

    Add ESLint configuration to disable 'react/no-unescaped-entities' rule

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // pode ter outras coisas aqui
+
+  // ADICIONE ESTE BLOCO:
+  eslint: {
+    rules: {
+      "react/no-unescaped-entities": "off"
+    },
+    // Isso é importante para garantir que o build não falhe
+    ignoreDuringBuilds: false, 
+  },
+};
 
 export default nextConfig;
